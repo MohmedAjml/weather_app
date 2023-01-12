de6e050dad93c293802336d9a79805a73e31bb31
ajmlmohmed <ajmalashraf983@gmail.com>
2023-01-12 13:34:55 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Wheather App',
+      home: Home(),
+    ));
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var temp;
+  var description;
+  var windspeed;
+  var Humidity;
+  var current;
+  var name;
+
+  Future getWeather() async {
+    http.Response response = await http.get(Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=kochi&units=metric&appid=5cf9ddd7f48d713ce871844cd7378acf"));
+
+    var results = jsonDecode(response.body);
+    setState(() {
+      this.temp = results['main']['temp'];
+      this.description = results['weather'][0]['description'];
+      this.current = results['weather'][0]['main'];
+      this.Humidity = results['main']['humidity'];
+      this.name = results['name'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(
+                      "https://cdn.britannica.com/74/182174-050-6755AB49/balloon-sky.jpg"),
+                  fit: BoxFit.cover),
+            ),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Text(
+                      "Current Weather in ${name}",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    temp != null ? temp.toString() + "\u00B0" : "Loading",
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  // Padding(
+                  //   padding: EdgeInsets.only(top: 10.0),
+                  //   child: Text(
+                  //     "Rain",
+                  //     style: TextStyle(
+                  //       color: Colors.white,
+                  //       fontSize: 16.0,
+                  //       fontWeight: FontWeight.w500,
+                  //     ),
+                  //   ),
+                  // ),
+                ]),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: const FaIcon(FontAwesomeIcons.thermometerHalf),
+                    iconColor: Colors.red,
+                    title: const Text('Temperature'),
+                    trailing: Text(
+                        temp != null ? temp.toString() + "\u00B0" : "Loading"),
+                  ),
+                  ListTile(
+                    leading: const FaIcon(FontAwesomeIcons.cloud),
+                    iconColor: Colors.teal,
+                    title: const Text('Description'),
+                    trailing: Text(description != null
+                        ? description.toString()
+                        : "Loading"),
+                  ),
+                  ListTile(
+                    leading: const FaIcon(FontAwesomeIcons.droplet),
+                    title: const Text('Humidity'),
+                    iconColor: Colors.blue,
+                    trailing: Text(
+                        Humidity != null ? Humidity.toString() : "Loading"),
+                  ),
+                  ListTile(
+                    leading: const FaIcon(FontAwesomeIcons.wind),
+                    title: const Text('Wind Speed'),
+                    iconColor: Colors.amber,
+                    trailing: Text(
+                        windspeed != null ? windspeed.toString() : "Loading"),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
